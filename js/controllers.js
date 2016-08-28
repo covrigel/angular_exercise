7361eedb22974f9012e78add86655fc27fc06404
covrigel <alexandra.nemes1986@gmail.com>
2016-08-28 19:50:04 +0000

    tested linking data with json through controller first

--- js/controllers.js
+++ js/controllers.js
@@ -3,9 +3,30 @@ var myApp = angular.module("myApp", []);
 
 myApp.controller("MyController", function MyController($scope){
   // here we manually create a model
-  $scope.author = {
+  $scope.artists = [
+  {
     "name"    : "Alexandra",
+    "shortname": "alexandra_n",
     "title"   : "Jr Dev",
     "company" : "Dreamjob"
+  },
+  {
+    "name"    : "Vladimir",
+    "shortname": "vladimir_n",
+    "title"   : "QA Lead",
+    "company" : "Awesomejob"
+  },
+  {
+    "name"    : "Ileana",
+    "shortname": "ileana_n",
+    "title"   : "Pharmacist",
+    "company" : "Greatjob"
+  },
+  {
+    "name"    : "Grigore",
+    "shortname": "grigore_n",
+    "title"   : "Restaurant Manager",
+    "company" : "Greatrestaurant"
   }
+]
 });
