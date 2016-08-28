053f894b2e063b852b580bc9d150b90390794683
covrigel <alexandra.nemes1986@gmail.com>
2016-08-28 20:56:28 +0000

    transfered data reading from json file, introduced data binding for sorting data

--- js/controllers.js
+++ js/controllers.js
@@ -1,32 +1,10 @@
 // in the [] we usually put the dependencies
 var myApp = angular.module("myApp", []);
 
-myApp.controller("MyController", function MyController($scope){
+myApp.controller("MyController",["$scope", "$http", function($scope, $http){
   // here we manually create a model
-  $scope.artists = [
-  {
-    "name"    : "Alexandra",
-    "shortname": "alexandra_n",
-    "title"   : "Jr Dev",
-    "company" : "Dreamjob"
-  },
-  {
-    "name"    : "Vladimir",
-    "shortname": "vladimir_n",
-    "title"   : "QA Lead",
-    "company" : "Awesomejob"
-  },
-  {
-    "name"    : "Ileana",
-    "shortname": "ileana_n",
-    "title"   : "Pharmacist",
-    "company" : "Greatjob"
-  },
-  {
-    "name"    : "Grigore",
-    "shortname": "grigore_n",
-    "title"   : "Restaurant Manager",
-    "company" : "Greatrestaurant"
-  }
-]
-});
+  $http.get("js/data.json").success(function(data){
+    $scope.artists = data;
+    $scope.artistOrder = "name";
+  });
+}]);
